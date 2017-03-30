7d0af1df4d1026ffb348b8fabe05f897d78fb24b
Akira Aida <akira.aida@ericsson.com>
2017-03-30 14:44:57 -0400

    Average wait Time Calculation

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"er-wait-time/handler"
+	"er-wait-time/rsc"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -23,6 +24,23 @@ func NewHttpHandler() http.Handler {
 	router.HandleFunc("/clinics/{clinicId:[0-9]+}/patients/{patientNumber:[0-9]+}", handler.RemovePatient()).Methods("DELETE")
 
 	return router
+}
+
+// To be kept in memory
+var clinicVisitTimes map[rsc.Clinic][]time.Time
+
+// To be executed every time that a 'sign out' happens
+func calculateAvgVisitTime(clinic rsc.Clinic, patient rsc.Patient) float32 {
+	visitTimes := clinicVisitTimes[clinic]
+	currentVisitTime := patient.CheckoutTime - time.Now()
+
+	averageVisitTime := 0.0
+	for visitTime := range visitTimes {
+		averageVisitTime += visitTime
+	}
+	averageVisitTime += currentVisitTime
+	averageVisitTime /= len(visitTimes + 1)
+	return averageVisitTime
 }
 
 func rootHandler() http.HandlerFunc {
