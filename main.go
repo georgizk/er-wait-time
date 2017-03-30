4843f4e60466390f888b9358d918d324d40a4b8a
Georgi Kostadinov <georgi.kostadinov@ericsson.com>
2017-03-30 11:48:44 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func main() {
+	address := "0.0.0.0:8080"
+	handler := NewHttpHandler()
+	http.ListenAndServe(address, handler)
+}
+
+func NewHttpHandler() http.Handler {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", rootHandler()).Methods("GET")
+
+	return router
+}
+
+func rootHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("test"))
+	}
+}
