e73c50dafa69f4ebc6d23e53a31a5ebddf9fa9fd
Georgi Kostadinov <georgi.kostadinov@ericsson.com>
2017-03-30 12:50:20 -0400

    handler code

---
+++ handler/handler.go
@@ -0,0 +1,51 @@
+package handler
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+)
+
+type ApiResponseIf interface {
+	getCode() int
+	getError() error
+}
+type ApiResponse struct {
+	Code  int     `json:"-"`
+	Error *string `json:"error"`
+}
+
+func NewApiResponse(code int, e *string) ApiResponse {
+	return ApiResponse{Code: code, Error: e}
+}
+
+func (r ApiResponse) getCode() int {
+	return r.Code
+}
+
+func (r ApiResponse) getError() error {
+	if nil != r.Error {
+		return errors.New(*r.Error)
+	} else {
+		return nil
+	}
+}
+
+func DecodeHelper(r *http.Request, s interface{}) error {
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+	err := decoder.Decode(s)
+	if err != nil {
+		log.Println("[---] Decode error:", err)
+		return errors.New("Decode error")
+	}
+	return nil
+}
+
+func EncodeHelper(w http.ResponseWriter, s ApiResponseIf) {
+	encoder := json.NewEncoder(w)
+	w.WriteHeader(s.getCode())
+	w.Header().Set("Content-Type", "application/json")
+	encoder.Encode(s)
+}
